f0eff2d17680c2ca2815e103efe1844d82f4112c
purkayasta <pritompurkayasta@gmail.com>
2023-03-25 02:35:18 +0600

    trying out selectable query

---
+++ FatRepository.SQLServer.Test.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FatRepository.SQLServer;
+using Microsoft.EntityFrameworkCore;
+
+
+var context = new BloggingContext();
+
+var repo = new FatRepository<Blog>(context);
+
+var val = repo.Find(x => x.Name.Equals("asd"), nameof(Blog.Name));
+
+var val2 = repo.Find<BlogSummary>(x => x.Name.Equals("pritom"), x=> new { x.Name, x.PostCount});
+
+var blogSummaries = context.Blogs
+    .Select(blog => new BlogSummary
+    {
+        Name = blog.Name,
+        PostCount = blog.Posts.Count
+    })
+    .ToList();
+
+
+
+
+public class BlogSummary
+{
+    public string Name { get; set; }
+    public int PostCount { get; set; }
+}
+
+public class Blog
+{
+    public string Name { get; set; }
+    public int PostCount { get; set; }
+}
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+}
--- FatRepository.SQLServer/FatRepository.cs
+++ FatRepository.SQLServer/FatRepository.cs
@@ -7,7 +7,7 @@ using System.Linq.Expressions;
 
 namespace FatRepository.SQLServer
 {
-    internal class FatRepository<T> where T : class
+    public class FatRepository<T> where T : class
     {
         private readonly DbContext _dbContext;
         private readonly DbSet<T> _dbSet;
@@ -32,14 +32,14 @@ namespace FatRepository.SQLServer
 
         #endregion
 
-        #region Simple Find Methods
+        #region Simple FindOne Queries
 
         public T? FindOne(Expression<Func<T, bool>> query) => _dbSet.Where(query).FirstOrDefault();
         public Task<T?> FindOneAsync(Expression<Func<T, bool>> query) => _dbSet.Where(query).FirstOrDefaultAsync();
 
         #endregion
 
-        #region Find Queries With Include
+        #region FindOne Queries With Include
 
         public T? FindOne(Expression<Func<T, bool>> query, params string[] includableMembers)
         {
@@ -65,5 +65,50 @@ namespace FatRepository.SQLServer
 
         #endregion
 
+        #region Simple Find Queries
+
+        public List<T> Find(Expression<Func<T, bool>> query) => _dbSet.Where(query).ToList();
+        public Task<List<T>> FindAsync(Expression<Func<T, bool>> query) => _dbSet.Where(query).ToListAsync();
+
+        #endregion
+
+        #region Find Queries with Include
+
+        public List<T> Find(Expression<Func<T, bool>> query, params string[] includableMembers)
+        {
+            if (includableMembers.Length < 1) throw new ArgumentException($"{nameof(includableMembers)} is empty");
+
+            IQueryable<T> findQuery = _dbSet.Where(query);
+
+            foreach (string member in includableMembers) findQuery.Include(member);
+
+            return findQuery.ToList();
+        }
+
+        public Task<List<T>> FindAsync(Expression<Func<T, bool>> query, params string[] includableMembers)
+        {
+            if (includableMembers.Length < 1) throw new ArgumentException($"{nameof(includableMembers)} is empty");
+
+            IQueryable<T> findQuery = _dbSet.Where(query);
+
+            foreach (string member in includableMembers) findQuery.Include(member);
+
+            return findQuery.ToListAsync();
+        }
+
+        #endregion
+
+        public List<TResult> Find<TResult>(Expression<Func<T, bool>> query, Func<T, int, TResult> selector)
+            => _dbSet.Where(query).Select(selector).ToList();
+
+        public Task<List<TResult>> FindAsync<TResult>(Expression<Func<T, bool>> query, Func<T, TResult> selector)
+        {
+            //var a = _dbSet.Where(query).Select(selector).ToList();
+            //var aa = Activator.CreateInstance<TResult>();
+
+            //return _dbSet.Where(query).Select(selector);
+
+            //return Enumerable.Empty<TResult>().ToList();
+        }
     }
 }
