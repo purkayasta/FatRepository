a305a61eaee83fd3187ac3876780d2dac7b7dd65
purkayasta <pritompurkayasta@gmail.com>
2023-03-25 16:02:23 +0600

    added MS DI installer

---
+++ FatRepository.SQLServer/Installer/MSServiceContainer.cs
@@ -0,0 +1,15 @@
+﻿using FatRepository.SQLServer.Contracts;
+using FatRepository.SQLServer.Implementions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FatRepository.SQLServer.Installer
+{
+    public static class MSServiceContainer
+    {
+        public static void AddFatRepository(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IFatRepository<>), typeof(FatRepository<>));
+            services.AddScoped<IFatUnitOfWork, FatUnitOfWork>();
+        }
+    }
+}
