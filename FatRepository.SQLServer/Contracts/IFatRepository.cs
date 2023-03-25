80168d8b449306e894e9d43f872229c00bbf2995
purkayasta <pritompurkayasta@gmail.com>
2023-03-25 16:01:14 +0600

    Added public contracts

---
+++ FatRepository.SQLServer/Contracts/IFatRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+namespace FatRepository.SQLServer.Contracts
+{
+    public interface IFatRepository<T> where T : class
+    {
+        List<T> Find(Expression<Func<T, bool>> query);
+        List<T> Find(Expression<Func<T, bool>> query, int skip, int take);
+        List<T> Find(Expression<Func<T, bool>> query, params string[] includableMembers);
+        List<TResult> Find<TResult>(Expression<Func<T, bool>> query, Expression<Func<T, TResult>> selector);
+        List<TResult> Find<TResult>(Expression<Func<T, bool>> query, Expression<Func<T, TResult>> selector, int skip, int take, params string[] includableMembers);
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> query);
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> query, int skip, int take);
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> query, params string[] includableMembers);
+        Task<List<TResult>> FindAsync<TResult>(Expression<Func<T, bool>> query, Expression<Func<T, TResult>> selector);
+        Task<List<TResult>> FindAsync<TResult>(Expression<Func<T, bool>> query, Expression<Func<T, TResult>> selector, int skip, int take, params string[] includableMembers);
+        T? FindOne(Expression<Func<T, bool>> query);
+        T? FindOne(Expression<Func<T, bool>> query, params string[] includableMembers);
+        Task<T?> FindOneAsync(Expression<Func<T, bool>> query);
+        Task<T?> FindOneAsync(Expression<Func<T, bool>> query, params string[] includableMembers);
+        Task InsertAsync(IEnumerable<T> entites);
+        void InsertOne(T entity);
+        Task InsertOneAsync(T entities);
+        void InsertRange(IEnumerable<T> entities);
+        void Modify(T entity);
+        Task ModifyAsync(T entity);
+        void ModifyRange(IEnumerable<T> entities);
+    }
+}
---
+++ FatRepository.SQLServer/Contracts/IFatUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace FatRepository.SQLServer.Contracts
+{
+    public interface IFatUnitOfWork
+    {
+        ChangeTracker ChangeTracker { get; }
+
+        void CloseTransaction();
+        Task CloseTransactionAsync();
+        int Commit();
+        Task<int> CommitAsync();
+        IExecutionStrategy CreateStrategy();
+        bool DatabaseCreate();
+        Task<bool> DatabaseCreateAsync();
+        bool DatabaseDelete();
+        Task<bool> DatabaseDeleteAsync();
+        IDbContextTransaction OpenTransaction();
+        Task<IDbContextTransaction> OpenTransactionAsync();
+        void RevertTransaction();
+        Task RevertTransactionAsync();
+    }
+}
