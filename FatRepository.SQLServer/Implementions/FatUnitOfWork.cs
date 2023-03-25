d243dc411f0992a1b0e35797cc8c346d156565f3
purkayasta <pritompurkayasta@gmail.com>
2023-03-25 16:01:41 +0600

    added unit of work implementation

---
+++ FatRepository.SQLServer/Implementions/FatUnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using FatRepository.SQLServer.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace FatRepository.SQLServer.Implementions
+{
+    internal class FatUnitOfWork : IFatUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public FatUnitOfWork(DbContext dbContext) => _dbContext = dbContext;
+
+        public ChangeTracker ChangeTracker { get => _dbContext.ChangeTracker; }
+
+        public int Commit() => _dbContext.SaveChanges();
+        public Task<int> CommitAsync() => _dbContext.SaveChangesAsync();
+
+        public IDbContextTransaction OpenTransaction() => _dbContext.Database.BeginTransaction();
+        public void CloseTransaction() => _dbContext.Database.CommitTransaction();
+        public void RevertTransaction() => _dbContext.Database.RollbackTransaction();
+
+        public Task<IDbContextTransaction> OpenTransactionAsync() => _dbContext.Database.BeginTransactionAsync();
+        public Task CloseTransactionAsync() => _dbContext.Database.CommitTransactionAsync();
+        public Task RevertTransactionAsync() => _dbContext.Database.RollbackTransactionAsync();
+
+
+        public IExecutionStrategy CreateStrategy() => _dbContext.Database.CreateExecutionStrategy();
+        public bool DatabaseCreate() => _dbContext.Database.EnsureCreated();
+        public bool DatabaseDelete() => _dbContext.Database.EnsureDeleted();
+
+        public Task<bool> DatabaseCreateAsync() => _dbContext.Database.EnsureCreatedAsync();
+        public Task<bool> DatabaseDeleteAsync() => _dbContext.Database.EnsureDeletedAsync();
+    }
+}
